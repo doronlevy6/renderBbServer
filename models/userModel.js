f96daf8fb2f686af51cca6a1e79261c0b94de16a
doronlevy6 <doronlevy66@gmail.com>
2023-08-31 00:18:47 +0300

    db internal

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "bbdb_49xs_user",
-  host: "dpg-cjnpmufjbvhs738fpr8g-a.oregon-postgres.render.com",
+  host: "dpg-cjnpmufjbvhs738fpr8g-a/bbdb_49xs",
   database: "bbdb_49xs",
   password: "EY2mw1Er8zoQvDtqyZyUCftUkqUypLc0",
   port: 5432,
