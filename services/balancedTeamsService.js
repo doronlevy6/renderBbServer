18a9dea9adb862997ce6d3efd05c63bd1592e81e
doronlevy6 <doronlevy66#gmail.com>
2023-09-07 22:10:33 +0300

    only moshe

--- services/balancedTeamsService.js
+++ services/balancedTeamsService.js
@@ -15,11 +15,15 @@ const setBalancedTeams = async (io) => {
       AVG(pr.rebound_skills) as rebound_skills
   FROM next_game_enlistment n
   LEFT JOIN player_rankings pr ON n.username = pr.rated_username
+  WHERE pr.rater_username IN ('Moshe', 'doron')
   GROUP BY n.username;
   `
     );// WHERE pr.rater_username IN ('Moshe', 'doron')
     const players = result.rows;
-     
+    
+    
+    
+
     // Filter out players with null parameters
     const validPlayers = players.filter(
       (player) =>
