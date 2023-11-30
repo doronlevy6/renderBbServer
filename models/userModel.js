7c84ced26e6a6125cc4c42e15492942a76c21237
doron levy <doronl@ravtech.co.il>
2023-11-30 14:29:44 +0200

    changing db

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,35 @@
 // server/models/userModel.js
-const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "bbdb_49xs_user",
-  host: "dpg-cjnpmufjbvhs738fpr8g-a.oregon-postgres.render.com",
-  database: "bbdb_49xs",
-  password: "EY2mw1Er8zoQvDtqyZyUCftUkqUypLc0",
+const postgres = require('postgres');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const pool = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
   port: 5432,
-  ssl: {
-    rejectUnauthorized: false, // Use this only for development purposes to accept self-signed certificates
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
   },
 });
 
 module.exports = pool;
+
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "bbdb_49xs_user",
+//   host: "dpg-cjnpmufjbvhs738fpr8g-a.oregon-postgres.render.com",
+//   database: "bbdb_49xs",
+//   password: "EY2mw1Er8zoQvDtqyZyUCftUkqUypLc0",
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false, // Use this only for development purposes to accept self-signed certificates
+//   },
+// });
+
+// module.exports = pool;
